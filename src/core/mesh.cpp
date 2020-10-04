fb0436f1c03db53e577b506eee9fce0568509548
Kevin Masson <hi@oktomus.com>
2020-10-04 14:48:00 +0200

    Create a core static lib

---
+++ src/core/mesh.cpp
@@ -0,0 +1,5 @@
+#include "mesh.h"
+#include <iostream>
+
+void tryCoreMesh()
+{ std::cout << "Core lib working\n"; }
--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,10 +1,13 @@
 #include "mainwindow.h"
 
+#include "core/mesh.h"
+
 #include <iostream>
 #include <string>
 
 int main()
 {
+    tryCoreMesh();
     gui::MainWindow& window = gui::MainWindow::get_instance();
 
     window.init(800, 600);
